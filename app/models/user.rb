712e63e0a3f4519dbecaa792a1a33516929cdcf6
Shubh <shubh.scb@gmail.com>
2022-10-18 16:51:01 +0530

    CORRECTION

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,8 @@ class User < ApplicationRecord
   has_many :posts, dependent: :destroy, inverse_of: :user, foreign_key: 'author_id'
   has_many :comments, through: :posts
   has_many :likes, through: :posts, dependent: :destroy
-end
 
-def three_recent_posts
-  posts.order(created_at: :desc).limit(3)
+  def three_recent_posts
+    posts.order(created_at: :desc).limit(3)
+  end
 end
