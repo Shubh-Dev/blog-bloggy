41c8f04daece1f622290642e24f43896eb1a9de0
Shubh <shubh.scb@gmail.com>
2022-10-18 16:37:46 +0530

    CUSTOM METHOD IMPLIMENT

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,19 @@ class Post < ApplicationRecord
   belongs_to :user, optional: true, foreign_key: 'author_id'
   has_many :comments, dependent: :destroy, inverse_of: :post, foreign_key: 'post_id'
   has_many :likes, dependent: :destroy, inverse_of: :post, foreign_key: :post_id
+
+
+  after_save :update_posts_counter
+
+
+def update_posts_counter
+  user.increment!(:posts_counter)
 end
+
+ def show_recent_comments
+  comments.order(created_at: :desc).limit(5)
+end
+end
+
+
+
