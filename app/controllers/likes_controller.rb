bd4ede22386e13e4e9fe6ced73626dacf662e7fa
Shubh <shubh.scb@gmail.com>
2022-10-27 16:35:22 +0530

    CREATING NEW LIKE AND COMMENT SUCCESS

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,10 +1,11 @@
 class LikesController < ApplicationController
     def create
-     @likes = Post.find(params[:post_id]).increment!(:likes_counter)
-     if @likes.save
-         redirect_to user_post_comments_path(current_user, params[:post_id])
-      else
-         flash[:alert] = "Something went wrong"
-      end
+     @post = Post.find(params[:post_id])
+        @like = @post.likes.new(author_id: current_user.id)
+        if @like.save
+            redirect_to user_post_path(current_user, params[:post_id])
+        else
+            render 'posts/show'
+        end
     end
 end
