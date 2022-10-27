032f37c04d60989892922aad2451bfd27c3c0a55
Shubh <shubh.scb@gmail.com>
2022-10-27 17:22:38 +0530

    NEW ADD

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,16 @@
 class CommentsController < ApplicationController
+  def index
+    @post = Post.find(params[:post_id])
+    @comments = @post.comments
+  end
+
+  def new
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.new
+
+    render :new
+  end
+
   def create
     @comment = current_user.comments.new(comment_params)
     flash[:notice] = @comment.errors.full_messages.to_sentence.capitalize unless @comment.save
