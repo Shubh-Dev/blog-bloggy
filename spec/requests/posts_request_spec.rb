09546e2875d9e51be2aff3e808b44d482e60d25f
Shubh <shubh.scb@gmail.com>
2022-10-29 19:09:02 +0530

    MODEL AND REQUEST TEST REFACTOR AFTER CHANGES

--- spec/requests/posts_request_spec.rb
+++ spec/requests/posts_request_spec.rb
@@ -1,33 +1,58 @@
 require 'rails_helper'
 
 RSpec.describe 'Posts', type: :request do
-  describe 'GET /posts' do
-    before(:example) { get user_posts_path('2') }
-    it 'works! (now write some real specs)' do
-      get user_posts_path
-      expect(response).to have_http_status(200)
+  describe 'GET #index' do
+    let(:user) do
+      User.create!(
+        name: 'John',
+        photo: 'https://as2.ftcdn.net/v2/jpg/02/17/51/67/1000_F_217516770_nHjCK3C82B2ZUC3JB3qQs8W2BGLHxZfa.jpg',
+        bio: 'I am a software developer',
+        posts_counter: 0
+      )
     end
-
-    it 'returns the status :ok' do
-      get user_posts_path
+    before(:example) { get("/users/#{user.id}/posts") }
+    it 'should have a http success status' do
       expect(response).to have_http_status(:ok)
     end
 
-    it 'renders the index template' do
-      get user_posts_path
-      expect(response).to render_template('index')
+    it 'renders corret page template' do
+      expect(response.body).to include('John')
     end
+    
+    it 'renders index tempate' do
+      expect(response).to render_template(:index)
+    end
+    
+    
   end
 
   describe 'GET /show' do
-    before(:example) { get user_post_path('1', '2') }
-    it 'renders the show template' do
-      get user_post_path('2')
-      expect(response).to render_template('show')
+    let(:user) do
+      User.create!(
+        name: 'John',
+        photo: 'https://as2.ftcdn.net/v2/jpg/02/17/51/67/1000_F_217516770_nHjCK3C82B2ZUC3JB3qQs8W2BGLHxZfa.jpg',
+        bio: 'I am a software developer',
+        posts_counter: 0
+      )
     end
-    it 'return status :ok' do
-      get user_post_path('2')
+    let(:post) do
+      Post.create!(
+        title: 'My first post',
+        text: 'This is a post',
+        author_id: user.id
+      )
+    end
+    before(:example) { get("/users/#{user.id}/posts/#{post.id}") }
+    it 'should have a http success status' do
       expect(response).to have_http_status(:ok)
+    end
+
+    it 'renders corret page template' do
+      expect(response.body).to include('This is a post')
+    end
+    
+    it 'renders show tempate' do
+      expect(response).to render_template(:show)
     end
   end
 end
