fd09aa1c25986b67dfe89bc130a718c987d2875e
Shubh <shubh.scb@gmail.com>
2022-10-30 09:36:14 +0530

    FEW TESTS PASSED

--- spec/views/users/user_show_spec.rb
+++ spec/views/users/user_show_spec.rb
@@ -2,26 +2,38 @@ require 'rails_helper'
 
 RSpec.describe 'user', type: :system do
     describe 'user show page' do
-        it 'shows the user show page' do
-        visit user_path(1)
-        expect(page).to have_content 'Bloggy App'
+        before :all do
+            @user = User.create(name: 'Victor', photo: 'https://i.imgur.com/1J8mYQJ.jpg', bio: 'I am a software developer', posts_counter: 0)
         end
-    
-        it 'shows the user show page' do
-        visit user_path(1)
-        user = User.find(1)
-        expect(page).to have_content user.name
+        after :all do
+            @user.destroy
         end
-    
-        it 'we can see photo' do
-        visit user_path(1)
-        page.has_selector?('img')
+
+        it 'shows the user name' do
+            visit user_path(@user)
+            expect(page).to have_content(@user.name)
         end
-    
-        it 'can see number of posts' do
-        visit user_path(1)
-        user = User.find(1)
-        expect(page).to have_content user.posts.count
+
+        it 'shows the user photo' do
+            visit user_path(@user)
+            expect(page).to have_css("img[
+            src='https://i.imgur.com/1J8mYQJ.jpg']")
         end
+
+        it 'shows the user bio' do
+            visit user_path(@user)
+            expect(page).to have_content(@user.bio)
+        end
+
+        it 'I can see the number of posts' do
+            visit user_path(@user)
+            expect(page).to have_content('Total Post: 0')
+        end
+
+        it 'I can see user\'s first three posts' do
+            visit user_path(@user)
+            expect(page).to have_content('No posts yet')
+        end
+           
     end
-    end
+end
