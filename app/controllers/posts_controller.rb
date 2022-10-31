6bc39499cff896b440816bc8be1a392730d8d66d
Shubh <shubh.scb@gmail.com>
2022-10-31 12:40:22 +0530

    INCLUDES METHOD ADD IN POST CONTROLLER

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,7 @@
 class PostsController < ApplicationController
   def index
     @user = User.find(params[:user_id])
-    @posts = Post.where(user: @user).order(created_at: :desc)
+    @posts = Post.includes(:user).where(user: @user).order(created_at: :desc)
   end
 
   def show
