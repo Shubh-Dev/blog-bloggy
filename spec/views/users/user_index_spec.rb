4c7a95ff5b7cb7508c4eec1338de9d3d9575011c
Shubh <shubh.scb@gmail.com>
2022-10-31 14:49:01 +0530

    REDIRECT TO SHOW PAGE TEST ADD

--- spec/views/users/user_index_spec.rb
+++ spec/views/users/user_index_spec.rb
@@ -2,6 +2,15 @@ require 'rails_helper'
 
 RSpec.describe 'user', type: :system do
   describe 'user index page' do
+    before :all do
+      @user = User.create(name: 'Victor', photo: 'https://i.imgur.com/1J8mYQJ.jpg',
+                          bio: 'I am a software developer', posts_counter: 0)
+      end
+
+      after :all do
+        @user.destroy
+      end
+
     it 'shows the user index page' do
       visit users_path
       expect(page).to have_content 'Bloggy App'
@@ -27,5 +36,12 @@ RSpec.describe 'user', type: :system do
         expect(page).to have_content user.posts.count
       end
     end
+
+    it 'user can redirect to the posts they made' do
+      visit users_path
+      click_link @user.name
+      expect(page).to have_content 'Total Post: 0'
+    end
+
   end
 end
