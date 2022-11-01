f1085de5b64cd1eebb343fb2183394b3e4ae0d16
Shubh <shubh.scb@gmail.com>
2022-11-01 12:18:01 +0530

    SIGNED IN SUCCESSFULLY

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root 'users#index'
+  
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :show, :new, :create] do
       resources :comments, only: [:index, :create, :new]
@@ -11,4 +11,5 @@ Rails.application.routes.draw do
     end
   end
   # root "articles#index"
+  root to: 'users#index'
 end
