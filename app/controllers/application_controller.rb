2b6fae6e6b76e28a10aa55fc9793cc761b7a0caa
Shubh <shubh.scb@gmail.com>
2022-11-01 15:15:20 +0530

    SIGN IN SIGN OUT SUCCESS

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,15 @@
 class ApplicationController < ActionController::Base
-  # before_action :authenticate_user!
+  before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-  # def route_not_found
-  #   render file: 'public/404.html', status: :not_found, layout: false
-  # end
+  def after_sign_out_path_for(_resource_or_scope)
+    new_user_session_path
+  end
+
+
+  def route_not_found
+    render file: 'public/404.html', status: :not_found, layout: false
+  end
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: %i[name bio photo])
