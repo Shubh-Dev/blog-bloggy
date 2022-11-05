de860992ce14aa7209fb1bec5a7253f7888a4623
Shubh <shubh.scb@gmail.com>
2022-11-05 14:16:10 +0530

    STYLE ADD

--- app/controllers/api/v1/application_controller.rb
+++ app/controllers/api/v1/application_controller.rb
@@ -1,2 +1,13 @@
 class Api::V1::ApplicationController < ActionController::API
+    def authenticate_user
+        return unless request.headers['Authorization'].present?
+    
+        begin
+          jwt_payload = JWT.decode(request.headers['Authorization'].split[1].remove('"'),
+                                   Rails.application.secrets.secret_key_base).first
+          @current_user_id = jwt_payload['id']
+        rescue JWT::ExpiredSignature, JWT::VerificationError, JWT::DecodeError
+          head :unauthorized
+        end
+      end
 end
